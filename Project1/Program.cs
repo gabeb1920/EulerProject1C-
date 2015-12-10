928cb0cff4f599a7371a25d4fb074b43471bb2a5
Gabriel Beedles <gabe@gabeb1920.com>
2015-12-11 08:26:23 +1100

    Solved

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -10,6 +10,21 @@ namespace Project1
     {
         static void Main(string[] args)
         {
+            int totalSum = CalculateSumOfMultiples(3, 1000) + CalculateSumOfMultiples(5, 1000) - CalculateSumOfMultiples(15, 1000);
+            Console.WriteLine("The sum of all multiples of 3 and 5 below 1000 is: " + totalSum);
+            Console.ReadLine();
+        }
+
+        private static int CalculateSumOfMultiples(int divisor, int max)
+        {
+            int sum = 0;
+
+            for(int i = 0; i * divisor < max; ++i)
+            {
+                sum += i * divisor;
+            }
+
+            return sum;
         }
     }
 }
